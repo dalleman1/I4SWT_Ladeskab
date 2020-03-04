fea9ea2f2587ba62559187c0d766a46fb4c13a7f
dalleman1 <monglade.dalsgaard@gmail.com>
2020-03-04 11:25:30 +0100

    Add structure

--- LadeskabCore/Door/DoorTriggeredEventArgs.cs
+++ LadeskabCore/Door/DoorTriggeredEventArgs.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LadeskabCore
+namespace LadeskabCore.Door
 {
-    public class Class1
+    class DoorTriggeredEventArgs
     {
     }
 }
