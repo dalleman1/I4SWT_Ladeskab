184da5f2b01d672f17d290b3172ac3cc4a9e63e6
dalleman1 <monglade.dalsgaard@gmail.com>
2020-03-10 13:05:52 +0100

    Add Eventreciever functionality for door

---
+++ LadeskabCore/StationControl/IStationControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LadeskabCore.StationControl
+{
+    public interface IStationControl
+    {
+        void LockDoor();
+
+        void UnlockDoor();
+    }
+}
---
+++ LadeskabCore/StationControl/StationControl.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LadeskabCore.Door;
+
+namespace LadeskabCore.StationControl
+{
+    public class StationControl : IStationControl
+    {
+        public bool currentStatus { get; set; }
+
+        public StationControl(IDoor Door)
+        {
+            Door.DoorChangedEvent += HandleStatusChangedEvent;
+        }
+
+        public void LockDoor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnlockDoor()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void HandleStatusChangedEvent(object sender, DoorTriggeredEventArgs e)
+        {
+            currentStatus = e.Status;
+
+            if (currentStatus == true)
+            {
+                UnlockDoor();
+            }
+            else
+            {
+                LockDoor();
+            }
+        }
+    }
+}
